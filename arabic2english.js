3be5be874358d4f162f8e017c42fde9dfe10e58d
Lalo Martins <lalo.martins@gmail.com>
2019-02-12 21:13:26 +0200

    initial commit

---
+++ arabic2english.js
@@ -0,0 +1,14 @@
+function number2English(num) {
+}
+
+function processCommandLine() {
+}
+
+module.exports = {
+  number2English,
+  processCommandLine,
+};
+
+if (require.main === module) {
+  processCommandLine();
+}
---
+++ arabic2english.test.js
@@ -0,0 +1,5 @@
+const program = require('./arabic2english');
+
+test('Processes command line', () => {
+  expect(true).toBe(false);
+});
