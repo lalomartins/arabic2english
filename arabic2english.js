decc66a678f5861849b92f95bcc3a3254cf2ba6f
Lalo Martins <lalo.martins@gmail.com>
2019-02-12 22:48:32 +0200

    implement up to 999

--- arabic2english.js
+++ arabic2english.js
@@ -1,7 +1,7 @@
 const dictionary = require('./dictionary');
 
 const BOUND_LOWER = 0;
-const BOUND_UPPER = 100;
+const BOUND_UPPER = 1000;
 
 function number2English(num) {
   if (num >= BOUND_UPPER || num < BOUND_LOWER) {
@@ -18,6 +18,16 @@ function number2English(num) {
       return dictionary.tens[Math.floor(num / 10) % 10];
     } else {
       return `${dictionary.tens[Math.floor(num / 10) % 10]}-${number2English(num % 10)}`;
+    }
+  }
+  if (num < 1000) {
+    const hundreds = `${number2English(Math.floor(num / 100))} hundred`;
+    if (num % 100 === 0) {
+      return hundreds;
+    } else if (num % 100 < 20 || num % 10 === 0) {
+      return `${hundreds} and ${number2English(num % 100)}`;
+    } else {
+      return `${hundreds} ${number2English(num % 100)}`;
     }
   }
 }
