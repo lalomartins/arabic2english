4d312f421abeb2d5c6ba948a9f3bdbb765be245f
Lalo Martins <lalo.martins@gmail.com>
2019-02-12 22:40:08 +0200

    refactor words into separate module

--- arabic2english.js
+++ arabic2english.js
@@ -1,60 +1,23 @@
+const dictionary = require('./dictionary');
+
 const BOUND_LOWER = 0;
 const BOUND_UPPER = 100;
-
-const SINGLE_DIGITS = [
-  'zero',
-  'one',
-  'two',
-  'three',
-  'four',
-  'five',
-  'six',
-  'seven',
-  'eight',
-  'nine',
-];
-
-const TEENS = [
-  'ten',
-  'eleven',
-  'twelve',
-  'thirteen',
-  'fourteen',
-  'fifteen',
-  'sixteen',
-  'seventeen',
-  'eighteen',
-  'nineteen',
-];
-
-const TENS = [
-  'zero',
-  'ten',
-  'twenty',
-  'thirty',
-  'fourty',
-  'fifty',
-  'sixty',
-  'seventy',
-  'eighty',
-  'ninety',
-];
 
 function number2English(num) {
   if (num >= BOUND_UPPER || num < BOUND_LOWER) {
     throw('Input out of bounds');
   }
   if (num < 10) {
-    return SINGLE_DIGITS[num];
+    return dictionary.singles[num];
   }
   if (num < 20) {
-    return TEENS[num - 10];
+    return dictionary.teens[num - 10];
   }
   if (num < 100) {
     if (num % 10 === 0) {
-      return TENS[Math.floor(num / 10) % 10];
+      return dictionary.tens[Math.floor(num / 10) % 10];
     } else {
-      return `${TENS[Math.floor(num / 10) % 10]}-${number2English(num % 10)}`;
+      return `${dictionary.tens[Math.floor(num / 10) % 10]}-${number2English(num % 10)}`;
     }
   }
 }
---
+++ dictionary.js
@@ -0,0 +1,40 @@
+module.exports = {
+  singles: [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ],
+
+  teens: [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ],
+
+  tens: [
+    'zero',
+    'ten',
+    'twenty',
+    'thirty',
+    'fourty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ],
+};
