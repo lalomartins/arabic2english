e75676aec90945d957d21ca1baf066d085780b21
Lalo Martins <lalo.martins@gmail.com>
2019-02-12 21:52:12 +0200

    process command line

--- arabic2english.js
+++ arabic2english.js
@@ -2,6 +2,14 @@ function number2English(num) {
 }
 
 function processCommandLine() {
+  const args = process.argv.slice(2);
+  for (const arg of args) {
+    if (isNaN(arg)) {
+      process.stderr.write(`Invalid input: '${arg}'\n`);
+    } else {
+      process.stdout.write(m.number2English(Number(arg)) + '\n');
+    }
+  }
 }
 
 const m = module.exports = {
